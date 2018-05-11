97063beb21274645fa98a4460001099ccc8e3073
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-11 16:14:48 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,7 +11,7 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   count = 0
 loop do 
-  puts "Welcome to Flatiron School's Web Development Course!"
+  puts "#{phrase}"
   count += 1 
   if count >= number_of_times 
     break
